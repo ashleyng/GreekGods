792a2d64d2a17547448ec79398075b645dec46ce
ashleyng <ashley.ng@utexas.edu>
2015-06-01 20:26:52 -0500

    simple tableview populated

---
+++ Greek-Gods/GreekGodsTVC.m
@@ -0,0 +1,103 @@
+//
+//  GreekGodsTVC.m
+//  Greek-Gods
+//
+//  Created by Ashley Ng on 6/1/15.
+//  Copyright (c) 2015 Ashley Ng. All rights reserved.
+//
+
+#import "GreekGodsTVC.h"
+#import <Firebase/Firebase.h>
+
+@interface GreekGodsTVC ()
+
+@property (nonatomic, strong) NSDictionary *data;
+@property (nonatomic, strong) NSArray *keys;
+
+@end
+
+@implementation GreekGodsTVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.title = @"Greek Gods";
+    Firebase *ref = [[Firebase alloc] initWithUrl:@"https://greek-gods.firebaseio.com/"];
+    
+    // get and store data from database
+    [ref observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        //        NSLog(@"%@", snapshot.value);
+        self.data = snapshot.value;
+        self.keys = [self.data allKeys];
+        NSLog(@"Data successfully retieved and stored");
+        [self.tableView reloadData];
+    } withCancelBlock:^(NSError *error) {
+        NSLog(@"%@", error.description);
+    }];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    NSLog(@"Number of Sections");
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    NSLog(@"Number of rows");
+    return [self.data count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Greek God Cell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    cell.textLabel.text = self.keys[indexPath.row];
+    cell.detailTextLabel.text = @"";
+    
+    
+    return cell;
+}
+
+
+
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
