49f2422d0685c529e931c3cd860084ffffe2829a
ashleyng <ashley.ng@utexas.edu>
2015-06-06 16:03:37 -0500

    add pods and parse set up

--- greekgods-parse/AppDelegate.m
+++ greekgods-parse/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+# warning fill this out with your own IDs and keys
+    [Parse setApplicationId:@""
+                  clientKey:@""];
+    
     return YES;
 }
 
