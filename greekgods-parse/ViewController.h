0c16a52b40f5204a5e698d963e210d18669e1cab
ashleyng <ashley.ng@utexas.edu>
2015-06-06 15:17:03 -0500

    initial commit

---
+++ greekgods-parse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  greekgods-parse
+//
+//  Created by Ashley Ng on 6/6/15.
+//  Copyright (c) 2015 Ashley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ greekgods-parseTests/greekgods_parseTests.m
@@ -0,0 +1,40 @@
+//
+//  greekgods_parseTests.m
+//  greekgods-parseTests
+//
+//  Created by Ashley Ng on 6/6/15.
+//  Copyright (c) 2015 Ashley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface greekgods_parseTests : XCTestCase
+
+@end
+
+@implementation greekgods_parseTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
